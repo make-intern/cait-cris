e8dc1060a118cad150909a8eb69a025eca99d691
dev.intern@make.technology <caitcelberry123@gmail.com>
2019-12-17 16:11:07 +0800

    FEAT: gallery component updated

---
+++ gallery.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'src/includes/head.php';?>
+<body>
+
+    <section class="o-container">
+        <div class="c-gallery">
+            <div class="c-gallery__col [ u-df-mb u-df-mb-fd-c u-df-dt-fd-r ] ">
+                <div class="c-card c-card--gallery " style="background-image: url('https://www.outbounders.tv/wp-content/uploads/2016/09/arca-1.jpg')">
+                    <div class="c-card__content">
+                        <h3>Arca South</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div>
+                <div class="c-card c-card--gallery " style="background-image: url('https://makeitmakati.com/wp-content/uploads/sites/14/2018/09/008.jpg')">
+                    <div class="c-card__content">
+                        <h3>Ayala Alabang</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div>
+                <div class="c-card c-card--gallery " style="background-image: url('https://i.ytimg.com/vi/3IZnSPnX5ew/maxresdefault.jpg')">
+                    <div class="c-card__content">
+                        <h3>Bonifacio Global City</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div>
+                <div class="c-card c-card--gallery " style="background-image: url('https://makeitmakati.com/wp-content/uploads/sites/14/2017/11/929f1085ceae33470b4480411032e845fa90f659.jpeg')">
+                    <div class="c-card__content">
+                        <h3>Circuit Makati</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div>            
+            </div>
+            <div class="c-gallery__col [ u-df-mb u-df-mb-fd-c u-df-dt-fd-r ] ">
+                <div class="c-card c-card--gallery " style="background-image: url('http://www.ayalaland.com.ph/wp-content/uploads/2016/08/Cloverleaf-Balintawak-Landscape-Promenade.jpg')">
+                    <div class="c-card__content">
+                        <h3>Cloverleaf</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div>
+                <div class="c-card c-card--gallery " style="background-image: url('https://www.discoveryprimea.com/files/2018/07/ayala-triangle-gardens-1.jpg')">
+                    <div class="c-card__content">
+                        <h3>Makati Central Business District</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div>
+                <div class="c-card c-card--gallery " style="background-image: url('https://toprealty.com.ph/wp-content/uploads/2018/11/parklinks-townships-preselling-condominiums-for-sale-in-pasig-and-quezon-city-1024x508.jpeg')">
+                    <div class="c-card__content">
+                        <h3>Parklinks</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div>
+                <div class="c-card c-card--gallery " style="background-image: url('https://kmcmaggroup.com/images/buildings/2613/perspective---south-park-corporate-center_highres.jpg')">
+                    <div class="c-card__content">
+                        <h3>South Park District</h3>
+                        <button class="o-button--icon">View Estate</button>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </section>  
+</body>
+</html>
