6fc7add27824eba7ddc625be060784c2ddcd6e08
dev.intern@make.technology <caitcelberry123@gmail.com>
2019-12-17 16:02:11 +0800

    FEAT: report box component updated

---
+++ report.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'src/includes/head.php';?>
+<body>
+    <section class="o-container o-container--report">
+        <figure>
+            <img src="https://www.ayalalandpremier.com/wp-content/uploads/2018/10/Hero-Shot.001.jpeg" alt="ayala land">
+        </figure>
+        <div class="c-report c-report--box [ u-df-mb u-df-mb-fw-w u-df-mb-fd-c u-df-mb-jc-sb u-df-tb-fd-r u-df-tb-ai-c ]  ">
+            <div class="c-report__text">
+                <!-- <h3>Annual Report</h3>-->
+                <h1>Enhancing land all over the country with estates</h1>
+                <p>Ayala Land recognizes that the value of land lies in how can it be sustained and enriched for future generations to enjoy.</p>
+                <!--<button class="o-button--bordered"><span></span><span></span><span class="text">Download Annual Report</span></button>-->
+            </div>
+            <div class="c-count c-count--box [ u-df-mb u-df-mb-jc-se u-df-mb-fd-c u-df-tb-fd-r ]">
+                <div class="c-count__item">
+                    <div class="">
+                        000
+                    </div>
+                    <div class="">
+                        Lorem Ipsum Dolor Amet
+                    </div>
+                </div>
+                <div class="c-count__item">
+                    <div class="">
+                        000
+                    </div>
+                    <div class="">
+                        Lorem Ipsum Dolor Amet
+                    </div>
+                </div>
+                <div class="c-count__item">
+                    <div class="">
+                        000
+                    </div>
+                    <div class="">
+                        Lorem Ipsum Dolor Amet
+                    </div>
+                </div>
+                <div class="c-count__item">
+                    <div class="">
+                        000
+                    </div>
+                    <div class="">
+                        Lorem Ipsum Dolor Amet
+                    </div>
+                </div>       
+            </div>             
+        </div>
+    </section>
+</body>
+</html>
